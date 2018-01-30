5e04e99bcae85a060ebed58c2b7c2b2547ba947e
Mohammed Saquib <Mohammed.Saquib@nbucni.com>
2018-01-30 16:22:45 +0530

    Changes to Sikuli code

---
+++ src/test/java/nbcu/compass/amorttemplate/test/MainClass.java
@@ -0,0 +1,27 @@
+package nbcu.compass.amorttemplate.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+
+public class MainClass {
+
+	public static void main(String args[]) {
+		List<String> file = new ArrayList<String>();
+		File directory = new File(".");
+		String strBasepath;
+		try {
+			strBasepath = directory.getCanonicalPath();
+			file.add(strBasepath+File.separator+"testng.xml");
+		    TestNG testNG = new TestNG();
+		    testNG.setTestSuites(file);
+		    testNG.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

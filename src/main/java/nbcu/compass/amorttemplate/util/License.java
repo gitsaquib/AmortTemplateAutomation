26a9dea0cace114e80aa4306c9704f8d65be9948
Mohammed Saquib <Mohammed.Saquib@nbucni.com>
2018-01-31 12:04:19 +0530

    Changes for fixing UT defects

---
+++ src/main/java/nbcu/compass/amorttemplate/util/License.java
@@ -0,0 +1,27 @@
+package nbcu.compass.amorttemplate.util;
+
+public class License {
+	
+	private String tcId;
+	private String licenseType;
+	private String licenseAmount;
+	
+	public String getTcId() {
+		return tcId;
+	}
+	public void setTcId(String tcId) {
+		this.tcId = tcId;
+	}
+	public String getLicenseType() {
+		return licenseType;
+	}
+	public void setLicenseType(String licenseType) {
+		this.licenseType = licenseType;
+	}
+	public String getLicenseAmount() {
+		return licenseAmount;
+	}
+	public void setLicenseAmount(String licenseAmount) {
+		this.licenseAmount = licenseAmount;
+	}
+}
